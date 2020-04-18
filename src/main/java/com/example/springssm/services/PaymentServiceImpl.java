1b63eb9876581885cfc8eab7b69282293534af27
rajatthakur00@gmail.com <rajat@55>
2020-04-18 17:34:50 +0530

    initializing state machine with database

--- src/main/java/com/example/springssm/services/PaymentServiceImpl.java
+++ src/main/java/com/example/springssm/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.springssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,6 +25,7 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm =build(paymentId);
         return null;
     }
 
@@ -35,5 +37,17 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = paymentRepository.getOne(paymentId);
+        StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(paymentId));
+        sm.stop();
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+        sm.start();
+        return sm;
     }
 }
